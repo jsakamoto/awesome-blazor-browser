d6fb2d2bbc3dde4339700a9102e091e908618e32
jsakamoto <junichi.jp@gmail.com>
2021-05-09 21:19:38 +0900

    Change pre-rendering method to "BlazorWasmPreRendering.Build"

--- AwesomeBlazorBrowser/Program.cs
+++ AwesomeBlazorBrowser/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace AwesomeBlazorBrowser
 {
@@ -15,7 +11,7 @@ namespace AwesomeBlazorBrowser
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
