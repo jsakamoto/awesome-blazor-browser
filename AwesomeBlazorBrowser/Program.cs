fec07c5f01dad9f00515d526824762a222108f42
jsakamoto <junichi.jp@gmail.com>
2021-08-25 21:42:35 +0900

    Change the entry point style to C# 9 top-level statement.

--- AwesomeBlazorBrowser/Program.cs
+++ AwesomeBlazorBrowser/Program.cs
@@ -1,17 +1,7 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using AwesomeBlazorBrowser;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace AwesomeBlazorBrowser
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+await builder.Build().RunAsync();
