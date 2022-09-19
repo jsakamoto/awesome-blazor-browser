d11f1c8e1e15c5d021ebfdc83d996b0a6e244c86
jsakamoto <junichi.jp@gmail.com>
2022-09-19 22:14:56 +0900

    Fix: pre-rendering was failed.

--- AwesomeBlazorBrowser/Program.cs
+++ AwesomeBlazorBrowser/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<HelperScriptService>();
+ConfigureServices(builder.Services, builder.HostEnvironment);
 await builder.Build().RunAsync();
+
+void ConfigureServices(IServiceCollection services, IWebAssemblyHostEnvironment hostEnvironment)
+{
+    services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(hostEnvironment.BaseAddress) });
+    services.AddScoped<HelperScriptService>();
+}
